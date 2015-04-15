afaa17713813e09f5628f015395d6d4ac8542657
Brandon Surmanski <b.surmanski@gmail.com>
2015-04-15 16:06:45 -0400

    inv trig, alt operators, dark theme

--- app/src/main/java/com/bsurmanski/calculator/Calculator.java
+++ app/src/main/java/com/bsurmanski/calculator/Calculator.java
@@ -14,6 +14,7 @@ public class Calculator {
         SUB,
         ADD,
         POW,
+        MOD,
     }
 
     enum UnOp {
@@ -24,9 +25,20 @@ public class Calculator {
         ASIN,
         ACOS,
         ATAN,
+        SINH,
+        COSH,
+        TANH,
+        ASINH,
+        ACOSH,
+        ATANH,
+        FAC,
+        SQ,
         SQRT,
         LOG,
         LN,
+        EX,
+        TENX,
+        RAND,
     }
 
     MathContext mc;
@@ -34,12 +46,21 @@ public class Calculator {
     BigDecimal ans; // previous entry
     BigDecimal entry; // current entry
     BinOp currentOp;
+    boolean useDegrees = true;
 
     public Calculator() {
         mc = new MathContext(20);
         ans = new BigDecimal(0, mc);
         entry = new BigDecimal(0, mc);
         currentOp = BinOp.ADD;
+    }
+
+    public void setDegrees() {
+        useDegrees = true;
+    }
+
+    public void setRadians() {
+        useDegrees = false;
     }
 
     public void setOperator(BinOp op) {
@@ -63,12 +84,59 @@ public class Calculator {
             case POW:
                 ans = new BigDecimal(Math.pow(ans.doubleValue(), entry.doubleValue()), mc);
                 break;
+            case MOD:
+                ans = new BigDecimal(ans.doubleValue() % entry.doubleValue(), mc);
+                break;
         }
         entry = ans;
     }
 
+    static double logGamma(double x) {
+        double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
+        double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
+                + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
+                +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
+        return tmp + Math.log(ser * Math.sqrt(2 * Math.PI));
+    }
+
+    static double gamma(double x) { return Math.exp(logGamma(x)); }
+
+    private double factorial(double i) {
+        if(i < 0) {
+            throw new IllegalArgumentException("factorial must be > 0");
+        }
+
+        double ret = gamma(i+1);
+        if(i == Math.floor(i)) {
+            ret = Math.round(ret);
+        }
+        return ret;
+    }
+
     void apply(UnOp op) {
+        double x = 0.0;
         switch(op) {
+            case FAC:
+                ans = new BigDecimal(factorial(entry.doubleValue()), mc);
+                break;
+            case SQRT:
+                ans = new BigDecimal(Math.sqrt(entry.doubleValue()), mc);
+                break;
+            case SQ:
+                ans = entry.multiply(entry, mc);
+                break;
+            case LOG:
+                ans = new BigDecimal(Math.log10(entry.doubleValue()), mc);
+                break;
+            case LN:
+                ans = new BigDecimal(Math.log(entry.doubleValue()), mc);
+                break;
+            case TENX:
+                ans = new BigDecimal(Math.pow(10, entry.doubleValue()), mc);
+                break;
+            case EX:
+                ans = new BigDecimal(Math.exp(entry.doubleValue()), mc);
+                break;
             case INV:
                 ans = new BigDecimal(1, mc).divide(ans, mc);
                 break;
@@ -78,28 +146,70 @@ public class Calculator {
             case COS:
                 ans = new BigDecimal(Math.cos(entry.doubleValue()), mc);
                 break;
+            case TAN:
+                ans = new BigDecimal(Math.tan(entry.doubleValue()), mc);
+                break;
+            case ASIN:
+                ans = new BigDecimal(Math.asin(entry.doubleValue()), mc);
+                break;
+            case ACOS:
+                ans = new BigDecimal(Math.acos(entry.doubleValue()), mc);
+                break;
+            case ATAN:
+                ans = new BigDecimal(Math.atan(entry.doubleValue()), mc);
+                break;
+            case SINH:
+                ans = new BigDecimal(Math.sinh(entry.doubleValue()), mc);
+                break;
+            case COSH:
+                ans = new BigDecimal(Math.cosh(entry.doubleValue()), mc);
+                break;
+            case TANH:
+                ans = new BigDecimal(Math.tanh(entry.doubleValue()), mc);
+                break;
+            case ASINH:
+                x = entry.doubleValue();
+                ans = new BigDecimal(Math.log(x + Math.sqrt(x*x + 1.0)), mc);
+                break;
+            case ACOSH:
+                x = entry.doubleValue();
+                ans = new BigDecimal(Math.log(x + Math.sqrt(x*x - 1.0)), mc);
+                break;
+            case ATANH:
+                x = entry.doubleValue();
+                ans = new BigDecimal(0.5*Math.log((x + 1.0) / (x - 1.0)), mc);
+                break;
+            case RAND:
+                ans = new BigDecimal(Math.random(), mc);
+                break;
         }
         entry = ans;
     }
 
     void clear() {
         entry = new BigDecimal(0, mc);
+        currentOp = BinOp.ADD;
     }
 
     void setEntry(String str) throws ParseException {
         ans = entry;
 
         BigDecimal val = new BigDecimal(0, mc);
-        boolean dot = false;
+        int dot = 0;
         boolean negative = false;
         for(int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if(c >= '0' && c <= '9') {
                 int digit = c - '0';
-                val = val.movePointRight(1);
-                val = val.add(new BigDecimal(digit, mc), mc);
-            } else if(c == '.' && !dot) {
-                dot = true;
+                if(dot == 0) {
+                    val = val.movePointRight(1);
+                    val = val.add(new BigDecimal(digit, mc), mc);
+                } else {
+                    val = val.add(new BigDecimal(digit, mc).movePointLeft(dot), mc);
+                    dot++;
+                }
+            } else if(c == '.' && dot == 0) {
+                dot = 1;
             } else if (c == '-' && i == 0) {
                 negative = true;
             } else {
@@ -110,6 +220,7 @@ public class Calculator {
             if(negative) {
                 val = val.negate();
             }
+
             entry = val;
         }
 
--- app/src/main/java/com/bsurmanski/calculator/MainActivity.java
+++ app/src/main/java/com/bsurmanski/calculator/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     Calculator calculator;
     TextView entryField;
     Button[] numberButtons;
+
+    // portrait buttons
     Button clearButton;
     Button negateButton;
     Button dotButton;
@@ -23,9 +25,29 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     Button mulButton;
     Button divButton;
     Button eqButton;
+
+    // landscape buttons
+    Button altButton;
+    Button randButton;
+    Button facButton;
+    Button modButton;
+    Button sqButton;
+    Button powButton;
+    Button logButton;
+    Button lnButton;
+    Button radButton;
+    Button piButton;
+    Button sinButton;
+    Button cosButton;
+    Button tanButton;
+    Button sinhButton;
+    Button coshButton;
+    Button tanhButton;
+
     String entryString;
 
     boolean clearOnNextInput;
+    boolean altOperator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +60,26 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         entryField.setText("" + calculator.getEntry());
         entryString = "0";
         clearOnNextInput = true;
+        altOperator = false;
 
+        // number buttons
+        numberButtons = new Button[10];
+        numberButtons[0] = (Button) findViewById(R.id.num0);
+        numberButtons[1] = (Button) findViewById(R.id.num1);
+        numberButtons[2] = (Button) findViewById(R.id.num2);
+        numberButtons[3] = (Button) findViewById(R.id.num3);
+        numberButtons[4] = (Button) findViewById(R.id.num4);
+        numberButtons[5] = (Button) findViewById(R.id.num5);
+        numberButtons[6] = (Button) findViewById(R.id.num6);
+        numberButtons[7] = (Button) findViewById(R.id.num7);
+        numberButtons[8] = (Button) findViewById(R.id.num8);
+        numberButtons[9] = (Button) findViewById(R.id.num9);
+
+        for(int i = 0; i < numberButtons.length; i++) {
+            numberButtons[i].setOnClickListener(this);
+        }
+
+        // portrait buttons
         clearButton = (Button) findViewById(R.id.cls);
         negateButton = (Button) findViewById(R.id.negate);
         dotButton = (Button) findViewById(R.id.dot);
@@ -57,21 +98,67 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         divButton.setOnClickListener(this);
         eqButton.setOnClickListener(this);
 
-        numberButtons = new Button[10];
-        numberButtons[0] = (Button) findViewById(R.id.num0);
-        numberButtons[1] = (Button) findViewById(R.id.num1);
-        numberButtons[2] = (Button) findViewById(R.id.num2);
-        numberButtons[3] = (Button) findViewById(R.id.num3);
-        numberButtons[4] = (Button) findViewById(R.id.num4);
-        numberButtons[5] = (Button) findViewById(R.id.num5);
-        numberButtons[6] = (Button) findViewById(R.id.num6);
-        numberButtons[7] = (Button) findViewById(R.id.num7);
-        numberButtons[8] = (Button) findViewById(R.id.num8);
-        numberButtons[9] = (Button) findViewById(R.id.num9);
+        //landscape buttons
+        altButton = (Button) findViewById(R.id.alt);
+        randButton = (Button) findViewById(R.id.rand);
+        piButton = (Button) findViewById(R.id.pi);
+        facButton = (Button) findViewById(R.id.fac);
+        modButton = (Button) findViewById(R.id.mod);
+        sqButton = (Button) findViewById(R.id.sq);
+        powButton = (Button) findViewById(R.id.pow);
+        radButton = (Button) findViewById(R.id.rad);
+        logButton = (Button) findViewById(R.id.log);
+        lnButton = (Button) findViewById(R.id.ln);
+        sinButton = (Button) findViewById(R.id.sin);
+        cosButton = (Button) findViewById(R.id.cos);
+        tanButton = (Button) findViewById(R.id.tan);
+        sinhButton = (Button) findViewById(R.id.sinh);
+        coshButton = (Button) findViewById(R.id.cosh);
+        tanhButton = (Button) findViewById(R.id.tanh);
 
-        for(int i = 0; i < numberButtons.length; i++) {
-            numberButtons[i].setOnClickListener(this);
-        }
+        altButton.setOnClickListener(this);
+        randButton.setOnClickListener(this);
+        piButton.setOnClickListener(this);
+        facButton.setOnClickListener(this);
+        modButton.setOnClickListener(this);
+        sqButton.setOnClickListener(this);
+        powButton.setOnClickListener(this);
+        radButton.setOnClickListener(this);
+        logButton.setOnClickListener(this);
+        lnButton.setOnClickListener(this);
+        sinButton.setOnClickListener(this);
+        cosButton.setOnClickListener(this);
+        tanButton.setOnClickListener(this);
+        sinhButton.setOnClickListener(this);
+        coshButton.setOnClickListener(this);
+        tanhButton.setOnClickListener(this);
+    }
+
+    void setAltLabels() {
+        sinButton.setText("ASIN");
+        cosButton.setText("ACOS");
+        tanButton.setText("ATAN");
+        sinhButton.setText("ASINH");
+        coshButton.setText("ACOSH");
+        tanhButton.setText("ACOSH");
+        lnButton.setText("e^x");
+        logButton.setText("10^x");
+        piButton.setText("e");
+        sqButton.setText("SQRT");
+    }
+
+    // instead of 'alt' labels of buttons
+    void setNormalLabels() {
+        sinButton.setText("SIN");
+        cosButton.setText("COS");
+        tanButton.setText("TAN");
+        sinhButton.setText("SINH");
+        coshButton.setText("COSH");
+        tanhButton.setText("COSH");
+        lnButton.setText("LN");
+        logButton.setText("LOG");
+        piButton.setText("PI");
+        sqButton.setText("X2");
     }
 
     int getButtonId(Button b) {
@@ -88,71 +175,175 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         return false;
     }
 
-    public void onClick(View v) {
-        if(entryString.equals("ERROR")) entryString = "0";
+    private void doUnaryOp(Calculator.UnOp op) {
+        try {
+            calculator.setEntry(entryString);
+            calculator.apply(op);
+            entryString = Double.toString(calculator.getEntry());
+            clearOnNextInput = true;
+        } catch(Exception e) {
+            entryString = "ERROR";
+            clearOnNextInput = true;
+        }
+    }
 
-        if(isNumericButton(v)) {
+    private void setBinaryOp(Calculator.BinOp op) {
+        try {
+            // allow operator chaining
+            calculator.setEntry(entryString);
+            calculator.apply();
+            entryString = Double.toString(calculator.getEntry());
+
+            calculator.setOperator(op);
+            calculator.setEntry(entryString);
+            clearOnNextInput = true;
+        } catch(Exception e){
+            entryString = "ERROR";
+            clearOnNextInput = true;
+        }
+    }
+
+    public void onClick(View v) {
+        if (entryString.equals("ERROR")) entryString = "0";
+
+        if (isNumericButton(v)) {
             int id = getButtonId((Button) v);
-            if(clearOnNextInput) {
+            if (clearOnNextInput) {
                 entryString = "";
                 clearOnNextInput = false;
             }
             entryString += Integer.toString(id);
         }
 
-        if(v == dotButton) {
-            if(!entryString.contains(".")) {
+        if (v == dotButton) {
+            if (!entryString.contains(".")) {
                 entryString += ".";
             }
         }
 
-        if(v == clearButton) {
+        if (v == clearButton) {
             calculator.clear();
             entryString = "0";
+            clearOnNextInput = true;
         }
 
-        try {
-            if (v == subButton) {
-                calculator.setOperator(Calculator.BinOp.SUB);
-                calculator.setEntry(entryString);
-                clearOnNextInput = true;
-            }
+        if (v == subButton) {
+            setBinaryOp(Calculator.BinOp.SUB);
+        }
 
-            if(v == addButton) {
-                calculator.setOperator(Calculator.BinOp.ADD);
-                calculator.setEntry(entryString);
-                clearOnNextInput = true;
-            }
+        if (v == addButton) {
+            setBinaryOp(Calculator.BinOp.ADD);
+        }
 
-            if(v == mulButton) {
-                calculator.setOperator(Calculator.BinOp.MUL);
-                calculator.setEntry(entryString);
-                clearOnNextInput = true;
-            }
+        if (v == mulButton) {
 
-            if(v == divButton) {
-                calculator.setOperator(Calculator.BinOp.DIV);
-                calculator.setEntry(entryString);
-                clearOnNextInput = true;
-            }
+            setBinaryOp(Calculator.BinOp.MUL);
+        }
 
-            if(v == eqButton) {
+        if (v == divButton) {
+            setBinaryOp(Calculator.BinOp.DIV);
+        }
+        if (v == eqButton) {
+            try {
                 calculator.setEntry(entryString);
                 calculator.apply();
                 entryString = Double.toString(calculator.getEntry());
                 clearOnNextInput = true;
+            } catch (Exception e) {
+                entryString = "ERROR";
+                clearOnNextInput = true;
             }
+        }
 
-            if(v == negateButton) {
-                if(entryString.startsWith("-")) {
-                    entryString = entryString.substring(1);
-                } else {
-                    entryString = "-" + entryString;
-                }
+        if (v == negateButton) {
+            if (entryString.startsWith("-")) {
+                entryString = entryString.substring(1);
+            } else {
+                entryString = "-" + entryString;
             }
+        }
 
-        } catch (Exception e) {
-            entryString = "ERROR";
+        // landscape buttons
+        if (v == altButton) {
+            altOperator = !altOperator;
+            if(altOperator) {
+                setAltLabels();
+            } else {
+                setNormalLabels();
+            }
+        }
+
+        if (v == randButton) {
+            doUnaryOp(Calculator.UnOp.RAND);
+        }
+
+        if (v == piButton) {
+            try {
+                if(altOperator) calculator.setEntry("2.718281828459045");
+                else calculator.setEntry("3.141592653589793");
+                entryString = Double.toString(calculator.getEntry());
+                clearOnNextInput = true;
+            } catch(Exception e) {
+                entryString = "ERROR";
+                clearOnNextInput = true;
+            }
+        }
+
+        if (v == facButton) {
+            doUnaryOp(Calculator.UnOp.FAC);
+        }
+
+        if(v == modButton) {
+            setBinaryOp(Calculator.BinOp.MOD);
+        }
+
+        if(v == powButton) {
+            setBinaryOp(Calculator.BinOp.POW);
+        }
+
+        if(v == sqButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.SQRT);
+            else doUnaryOp(Calculator.UnOp.SQ);
+        }
+
+        if (v == logButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.TENX);
+            else doUnaryOp(Calculator.UnOp.LOG);
+        }
+
+        if (v == lnButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.EX);
+        else doUnaryOp(Calculator.UnOp.LN);
+        }
+
+        if (v == sinButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.ASIN);
+            else doUnaryOp(Calculator.UnOp.SIN);
+        }
+
+        if (v == cosButton) {
+            if(altOperator)  doUnaryOp(Calculator.UnOp.ACOS);
+            else doUnaryOp(Calculator.UnOp.COS);
+        }
+
+        if (v == tanButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.ATAN);
+            else doUnaryOp(Calculator.UnOp.TAN);
+        }
+
+        if (v == sinhButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.ASINH);
+            else doUnaryOp(Calculator.UnOp.SINH);
+        }
+
+        if (v == coshButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.ACOSH);
+            else doUnaryOp(Calculator.UnOp.COSH);
+        }
+
+        if (v == tanhButton) {
+            if(altOperator) doUnaryOp(Calculator.UnOp.TANH);
+            else doUnaryOp(Calculator.UnOp.TANH);
         }
 
         entryField.setText(entryString);
